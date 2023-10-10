bb86cc883b4960e3107ff0ad5c8f960a4708c306
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-10-10 14:29:53 +0700

    fix bugs

--- app/model/index.js
+++ app/model/index.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
-// const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
-//     host: process.env.MYSQLHOST,
-//     port: process.env.MYSQLPORT,
-//     dialect: 'mysql'
-// });
+const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql'
+});
 
-const sequelize = new Sequelize("revou_jayapura", "root", "", {
-        host: "localhost",
-        dialect: 'mysql',
-        logging: false
-    });
+// const sequelize = new Sequelize("revou_jayapura", "root", "", {
+//         host: "localhost",
+//         dialect: 'mysql',
+//         logging: false
+//     });
 
 module.exports = sequelize;
